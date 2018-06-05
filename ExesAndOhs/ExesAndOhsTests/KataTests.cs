6cd0985c7797d7a379e527750c322481830ae6a2
Ouch.Liu <ouch.liu@xuenn.com>
2018-06-05 15:28:22 +0800

    Initial commit

---
+++ ExesAndOhs/ExesAndOhsTests/KataTests.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ExesAndOhs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExesAndOhs.Tests
+{
+    [TestClass()]
+    public class KataTests
+    {
+        [TestMethod()]
+        public void ExampleTests()
+        {
+            Assert.AreEqual( true , Kata.XO( "xo" ) );
+            Assert.AreEqual( true , Kata.XO( "xxOo" ) );
+            Assert.AreEqual( false , Kata.XO( "xxxm" ) );
+            Assert.AreEqual( false , Kata.XO( "Oo" ) );
+            Assert.AreEqual( false , Kata.XO( "ooom" ) );
+        }
+    }
+}
